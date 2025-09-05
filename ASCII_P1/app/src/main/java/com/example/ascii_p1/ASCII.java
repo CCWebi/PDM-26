73cce1fc0a801e556f2d17e159839606cd33b7d9
Isaac Robledo <isaacrobled@ciencias.unam.mx>
2025-09-04 22:05:55 -0600

    feat: first project

---
+++ ASCII_P1/app/src/main/java/com/example/ascii_p1/ASCII.java
@@ -0,0 +1,12 @@
+package com.example.ascii_p1;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ASCII extends Activity {
+    @Override
+    public void onCreate(Bundle saveInstanceState) {
+        super.onCreate(saveInstanceState);
+        setContentView(R.layout.main_layout);
+    }
+}
